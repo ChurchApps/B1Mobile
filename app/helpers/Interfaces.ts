ff5942c76618485260d82a82e417c2c1934449c9
Jeremy Zongker <jeremy@trilitech.com>
2021-02-24 01:15:11 -0600

    Updated ids to strings

--- app/helpers/Interfaces.ts
+++ app/helpers/Interfaces.ts
@@ -3,24 +3,24 @@ export type ApiListType = "AccessApi" | "MembershipApi" | "AttendanceApi" | "B1A
 
 
 //B1
-export interface LinkInterface { id?: number, churchId: number, category: string, url?: string, text?: string, sort?: number, linkType: string, linkData: string, icon: string }
-export interface PageInterface { id?: number, churchId: number, name: string, content: string }
+export interface LinkInterface { id?: string, churchId: string, category: string, url?: string, text?: string, sort?: number, linkType: string, linkData: string, icon: string }
+export interface PageInterface { id?: string, churchId: string, name: string, content: string }
 
 //AccessManagment
 export interface LoginRequestInterface { email: string, password: string }
 export interface ApiInterface { name: string, keyName?: string, permissions: RolePermissionInterface[], jwt: string }
 export interface ApplicationInterface { name: string, keyName?: string, permissions: RolePermissionInterface[] }
-export interface ChurchAppInterface { id?: number, churchId?: number, appName?: string }
-export interface ChurchInterface { id?: number, name: string, registrationDate?: Date, apis?: ApiInterface[], apps?: ApplicationInterface[], address1?: string, address2?: string, city?: string, state?: string, zip?: string, country?: string, subDomain?: string }
+export interface ChurchAppInterface { id?: string, churchId?: string, appName?: string }
+export interface ChurchInterface { id?: string, name: string, registrationDate?: Date, apis?: ApiInterface[], apps?: ApplicationInterface[], address1?: string, address2?: string, city?: string, state?: string, zip?: string, country?: string, subDomain?: string }
 export interface ForgotResponse { emailed: boolean }
 export interface LoadCreateUserRequestInterface { userEmail: string, fromEmail?: string, subject?: string, body?: string, userName: string }
 export interface LoginResponseInterface { user: UserInterface, churches: ChurchInterface[], errors: string[] }
 export interface PermissionInterface { apiName?: string, section?: string, action?: string, displaySection?: string, displayAction?: string }
 export interface RegisterInterface { churchName?: string, displayName?: string, email?: string, password?: string }
-export interface RoleInterface { id?: number, churchId?: number, appName?: string, name?: string }
-export interface RolePermissionInterface { id?: number, churchId?: number, roleId?: number, apiName?: string, contentType?: string, contentId?: number, action?: string }
-export interface RoleMemberInterface { id?: number, churchId?: number, roleId?: number, userId?: number, user?: UserInterface }
+export interface RoleInterface { id?: string, churchId?: string, appName?: string, name?: string }
+export interface RolePermissionInterface { id?: string, churchId?: string, roleId?: string, apiName?: string, contentType?: string, contentId?: string, action?: string }
+export interface RoleMemberInterface { id?: string, churchId?: string, roleId?: string, userId?: string, user?: UserInterface }
 export interface ResetPasswordRequestInterface { userEmail: string, fromEmail: string, subject: string, body: string }
 export interface ResetPasswordResponseInterface { emailed: boolean }
 
-export interface UserInterface { id?: number, email?: string, authGuid?: string, displayName?: string, registrationDate?: Date, lastLogin?: Date, password?: string }
+export interface UserInterface { id?: string, email?: string, authGuid?: string, displayName?: string, registrationDate?: Date, lastLogin?: Date, password?: string }
--- app/helpers/UserHelper.ts
+++ app/helpers/UserHelper.ts
@@ -6,29 +6,23 @@ export class UserHelper {
 
     static login = async (req: LoginRequestInterface) => {
         const resp: LoginResponseInterface = await ApiHelper.postAnonymous("/users/login", req, "AccessApi");
-        const success = await UserHelper.hanldeLogin(resp);
+        const success = await UserHelper.handleLogin(resp);
         return success;
     }
 
-    static hanldeLogin = async (resp: LoginResponseInterface): Promise<boolean> => {
-        console.log("RESPONSE");
-        console.log(resp);
+    static handleLogin = async (resp: LoginResponseInterface): Promise<boolean> => {
         if (resp === undefined || resp.errors?.length > 0) {
             return false;
         } else {
             CachedData.church = resp.churches[0];
             resp.churches[0].apis?.forEach(api => { ApiHelper.setPermissions(api.keyName || "", api.jwt, api.permissions); });
-            UserHelper.loadTabs();
+            await UserHelper.loadTabs();
             return true;
         }
     }
 
-
-    static loadTabs = () => {
-        ApiHelper.get("/links?category=tab", "B1Api").then((tabs: LinkInterface[]) => {
-            CachedData.tabs = tabs;
-
-        });
+    static loadTabs = async () => {
+        CachedData.tabs = await ApiHelper.get("/links/church/" + CachedData.church?.id + "?category=tab", "B1Api");
     }
 
 }
