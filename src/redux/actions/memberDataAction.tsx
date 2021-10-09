0e51b6cb8d2999bb3f2dd0ed64c54deb991f3411
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2021-10-09 17:13:27 +0530

    use /people/:id api for fetching person record (#9)

--- src/redux/actions/memberDataAction.tsx
+++ src/redux/actions/memberDataAction.tsx
@@ -2,13 +2,13 @@ import axios from "axios";
 import API from "../../helper/ApiConstants";
 import { MEMBER_DATA_FETCH, MEMBER_DATA_FETCH_FAILURE, MEMBER_DATA_FETCH_SUCCESS } from "../reducers/memberDataReducer";
 
-export function getMemberData(userId: any, token: any, callback: any) {
+export function getMemberData(personId: any, token: any, callback: any) {
     return (dispatch:any) => {
         dispatch({
             type: MEMBER_DATA_FETCH
         });
 
-        axios.get(API.MEMBER_URL + '/userId/' + userId, { headers: {"Authorization" : `Bearer ${token}`} })
+        axios.get(API.MEMBER_URL + '/' + personId, { headers: {"Authorization" : `Bearer ${token}`} })
             .then(res => {
                 dispatch({
                     type: MEMBER_DATA_FETCH_SUCCESS,
