da90da2d24e73de4ca671a5a9b352bf80cd2ce99
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-10-22 18:02:21 +0530

    add displayBox component

---
+++ src/components/DisplayBox.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+import { widthPercentageToDP as wp } from "react-native-responsive-screen";
+import globalStyles from "../helper/GlobalStyles";
+
+interface Props {
+  title: string;
+  rightHeaderComponent?: React.ReactNode;
+  headerIcon: React.ReactNode;
+  children: React.ReactNode;
+  footer: React.ReactNode;
+}
+
+export function DisplayBox({ title, rightHeaderComponent, headerIcon, children, footer }: Props) {
+  return (
+    <View style={globalStyles.paymentTitleContainer}>
+      <View style={{ width: wp("100%") }}>
+        <View style={globalStyles.paymentTitleHeaderLine} />
+        <View style={globalStyles.paymentTitleView}>
+          {headerIcon}
+          <Text style={globalStyles.paymentTitleText}>{title}</Text>
+          {rightHeaderComponent || <View style={{ width: wp("6%") }} />}
+        </View>
+      </View>
+      {children}
+      {footer}
+    </View>
+  );
+}
