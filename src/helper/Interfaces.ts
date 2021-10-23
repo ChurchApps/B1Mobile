b09f06dcbd076da62cb7bf49e7876b61e9bd3689
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-10-23 16:30:50 +0530

    Update churchInterface

--- src/helper/Interfaces.ts
+++ src/helper/Interfaces.ts
@@ -4,7 +4,7 @@ export type ApiListType = "AccessApi" | "MembershipApi" | "AttendanceApi" | "B1A
 export interface ApiInterface { name: string, keyName?: string, permissions: RolePermissionInterface[], jwt: string }
 export interface ApplicationInterface { name: string, keyName?: string, permissions: RolePermissionInterface[] }
 export interface ChurchAppInterface { id?: string, churchId?: string, appName?: string }
-export interface ChurchInterface { id?: string, name: string, registrationDate?: Date, apis?: ApiInterface[], apps?: ApplicationInterface[], address1?: string, address2?: string, city?: string, state?: string, zip?: string, country?: string, subDomain?: string }
+export interface ChurchInterface { id?: string, name?: string, registrationDate?: Date, apis?: ApiInterface[], address1?: string, address2?: string, city?: string, state?: string, zip?: string, country?: string, subDomain?: string, personId?: string, jwt?: string, settings?: GenericSettingInterface[] }
 export interface ForgotResponse { emailed: boolean }
 export interface LoadCreateUserRequestInterface { userEmail: string, fromEmail?: string, subject?: string, body?: string, userName: string }
 export interface LoginResponseInterface { user: UserInterface, churches: ChurchInterface[], errors: string[] }
@@ -18,6 +18,7 @@ export interface ResetPasswordResponseInterface { emailed: boolean }
 export interface SwitchAppRequestInterface { appName: string, churchId: string }
 export interface SwitchAppResponseInterface { appName: string, churchId: string }
 export interface UserInterface { id?: string, email?: string, authGuid?: string, displayName?: string, registrationDate?: Date, lastLogin?: Date, password?: string }
+export interface GenericSettingInterface { id?: string, churchId?: string, keyName?: string, value?: string, public?: number }
 
 export interface GroupServiceTimeInterface { id?: string, groupId?: string, serviceTimeId?: string, serviceTime?: ServiceTimeInterface }
 export interface GroupInterface { id?: string, name?: string, categoryName?: string, parentPickup?: boolean }
