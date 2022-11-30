546d34d898606441ad6d67a7af30056b6d19b476
Jeremy Zongker <jeremy@trilitech.com>
2022-11-30 15:19:12 -0600

    Fixed typo

--- src/screens/ChurchSearch.tsx
+++ src/screens/ChurchSearch.tsx
@@ -60,7 +60,7 @@ export const ChurchSearch = (props: Props) => {
 
   const searchApiCall = (text: String) => {
     setLoading(true);
-    ApiHelper.getAnonymous("/churches/search/?name=" + text + "&app=B1&include=logoSquare", "MembeshipApi").then(data => {
+    ApiHelper.getAnonymous("/churches/search/?name=" + text + "&app=B1&include=logoSquare", "MembershipApi").then(data => {
       setLoading(false);
       setSearchList(data);
       if (data.length === 0) Alert.alert("Alert", "No matches found");
