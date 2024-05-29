a599ec95d655b457298fab9078a11ef57e565759
Jeremy Zongker <jeremy@trilitech.com>
2024-05-28 19:38:39 -0500

    1.9.0 - Fixed broken lessons and plans (#65)
    * Updated plan details to allow anonymous users
    
    * Updated web browser and allowed for http
    
    * Deployed 1.9.0
    
    * Deployed 1.9.0
    
    * Deployed to Android store

--- src/helpers/NavigationHelper.ts
+++ src/helpers/NavigationHelper.ts
@@ -17,6 +17,7 @@ export class NavigationHelper {
     if (item.linkType == "lessons") {
       UserHelper.addOpenScreenEvent('LessonsScreen');
       const jwt = UserHelper.currentUserChurch?.jwt;
+      console.log("***** " + EnvironmentHelper.LessonsRoot + "/login?jwt=" + jwt + "&returnUrl=/b1/person&churchId=" + CacheHelper.church!.id);
       navigate('LessonsScreen', { url: EnvironmentHelper.LessonsRoot + "/login?jwt=" + jwt + "&returnUrl=/b1/person&churchId=" + CacheHelper.church!.id, title: item.text })
     }
     if (item.linkType == "bible") {
@@ -38,6 +39,7 @@ export class NavigationHelper {
     }
     if (item.linkType == "url") {
       UserHelper.addOpenScreenEvent('WebsiteScreen');
+      console.log("WEBSITE******* ", item.url);
       navigate('WebsiteScreen', { url: item.url, title: item.text })
     }
     if (item.linkType == "page") {
