b83435d1739af021b3fc0e9c59715790398adbe9
Jeremy Zongker <jeremy@zongker.net>
2024-08-16 11:34:03 -0500

    "Fixed" print for lessons

--- src/screens/WebsiteScreen.tsx
+++ src/screens/WebsiteScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { Linking, SafeAreaView, View } from 'react-native';
 import WebView from 'react-native-webview';
 import { Loader, MainHeader } from '../components';
 import { CacheHelper, Utilities, globalStyles } from '../helpers';
@@ -18,6 +18,8 @@ interface Props {
 export const WebsiteScreen = (props: Props) => {
   const { params } = props.route;
   const [isLoading, setLoading] = useState(false);
+  const [currentUrl, setCurrentUrl] = useState("");
+  const webviewRef = useRef<any>();
   //console.log(params?.url)
 
   const checkRedirect = () => {
@@ -34,12 +36,20 @@ export const WebsiteScreen = (props: Props) => {
     return title == undefined ? 'Home' : title;
   }
 
+  const handleMessage = (event: any) => {
+
+    console.log("MADE IT")
+    console.log("RECEIVED MESSAGE", event.nativeEvent.data);
+    let newUrl = currentUrl + "&autoPrint=1"
+    Linking.openURL(newUrl);
+  }
+
   return (
     <SafeAreaView style={globalStyles.homeContainer}>
       <MainHeader title={getTitle()} openDrawer={props.navigation.openDrawer} back={props.navigation.goBack}/>
       <>
         <View style={globalStyles.webViewContainer}>
-          <WebView onLoadStart={() => setLoading(true)} onLoadEnd={() => setLoading(false)} source={{ uri: params?.url }} scalesPageToFit={false} />
+          <WebView ref={webviewRef} onMessage={handleMessage} onLoadStart={() => setLoading(true)} onLoadEnd={() => setLoading(false)} onNavigationStateChange={(state:any) => { setCurrentUrl(state.url) }} source={{ uri: params?.url }} scalesPageToFit={false} />
         </View>
 
         {isLoading && <Loader isLoading={isLoading} />}
