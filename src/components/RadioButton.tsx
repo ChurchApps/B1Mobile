c6d1feae727f1e15cc0f2a711e5feb7c96a56c9b
Payalpie <payalpie0@gmail.com>
2024-12-24 18:13:36 +0530

    added create event functionality

---
+++ src/components/RadioButton.tsx
@@ -0,0 +1,75 @@
+import { DimensionHelper } from '@churchapps/mobilehelper';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface RadioButtonProps {
+  label: string;
+  value: string;
+  selectedValue: string;
+  onSelect: (value: string) => void;
+}
+
+function RadioButton({ label, value, selectedValue, onSelect }: RadioButtonProps) {
+  return (
+    <TouchableOpacity
+      style={styles.singleOptionContainer}
+      onPress={() => onSelect(value)}>
+      <View style={styles.outerCircle}>
+        {selectedValue === value ? <View style={styles.innerCircle} /> : null}
+      </View>
+      <Text style={{ fontSize: DimensionHelper.wp('3.5%') }}>{label}</Text>
+    </TouchableOpacity>
+  );
+}
+
+interface RadioButtonGroupProps {
+  options: string[]; // Display labels
+  values: string[]; // Actual values
+  selectedValue: string;
+  onValueChange: (value: string) => void;
+}
+
+export function RadioButtonGroup({
+  options,
+  values,
+  selectedValue,
+  onValueChange,
+}: RadioButtonGroupProps) {
+  return (
+    <View>
+      {options.map((option, index) => (
+        <RadioButton
+          key={values[index]}
+          label={option}
+          value={values[index]}
+          selectedValue={selectedValue}
+          onSelect={onValueChange}
+        />
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  singleOptionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: 10,
+    margin: 5,
+  },
+  outerCircle: {
+    width: 25,
+    height: 25,
+    borderRadius: 25 / 2,
+    borderWidth: 2,
+    borderColor: 'rgb(25, 118, 210)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerCircle: {
+    width: 14,
+    height: 14,
+    borderRadius: 14 / 2,
+    backgroundColor: 'rgb(25, 118, 210)]',
+  },
+});
