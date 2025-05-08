d2582245054c8118ea85596a2e663030cfc8239b
Jeremy Zongker <jeremy@zongker.net>
2025-05-07 20:52:32 -0500

    Revert "0.2.1 - Expo"
    This reverts commit b3f1b3e418a0c6bd4033c5b16d6bbda9e675322d.

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,14 @@
-module.exports =
-  function (
-    api
-  ) {
-    api.cache(
-      true
-    );
-    return {
-      presets:
-        [
-          "babel-preset-expo"
-        ],
-      plugins:
-        [
-          [
-            "react-native-reanimated/plugin"
-          ]
-        ]
-    };
-  };
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ["module:react-native-dotenv", {
+      "moduleName": "@env",
+      "path": ".env",
+      "blacklist": null,
+      "whitelist": null,
+      "safe": false,
+      "allowUndefined": true
+    },],
+    ["react-native-reanimated/plugin"]
+  ]
+};
--- src/types/expo-error-recovery.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'expo-error-recovery' {
-  export function setGlobalErrorHandler(handler: (error: Error, isFatal?: boolean) => void): void;
-  export function getGlobalErrorHandler(): ((error: Error, isFatal?: boolean) => void) | null;
-} 
