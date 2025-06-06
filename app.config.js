f0bdc99251cfb962cb565bb77cb75805fd643487
Jeremy Zongker <jeremy@zongker.net>
2025-06-05 22:41:13 -0500

    Fixing eas errors

--- app.config.js
+++ app.config.js
@@ -24,6 +24,7 @@ module.exports = {
     web: {
       favicon: "./assets/favicon.png"
     },
+    runtimeVersion: "2.0.0",
     extra: {
       ATTENDANCE_API: process.env.ATTENDANCE_API || "https://attendanceapi.churchapps.org",
       B1_WEB_ROOT: process.env.B1_WEB_ROOT || "https://{subdomain}.b1.church",
@@ -36,7 +37,10 @@ module.exports = {
       MEMBERSHIP_API: process.env.MEMBERSHIP_API || "https://membershipapi.churchapps.org",
       MESSAGING_API: process.env.MESSAGING_API || "https://messagingapi.churchapps.org",
       STREAMING_LIVE_ROOT: process.env.STREAMING_LIVE_ROOT || "https://{subdomain}.streaminglive.church",
-      STAGE: process.env.STAGE || "prod"
+      STAGE: process.env.STAGE || "prod",
+      eas: {
+        projectId: "f72e5911-b8d5-467c-ad9e-423c180e9938"
+      }
     }
   }
 };
